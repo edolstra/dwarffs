71bdbe2784336a317ce09e2bb256ee628e02f464
Eelco Dolstra <edolstra@gmail.com>
2017-07-06 18:47:16 +0200

    Fix default.nix

--- default.nix
+++ default.nix
@@ -6,4 +6,12 @@ stdenv.mkDerivation {
   buildInputs = [ fuse nixUnstable nlohmann_json ];
 
   NIX_CFLAGS_COMPILE = "-I ${nixUnstable.dev}/include/nix -include ${nixUnstable.dev}/include/nix/config.h -D_FILE_OFFSET_BITS=64";
+
+  src = lib.cleanSource ./.;
+
+  installPhase =
+    ''
+      mkdir -p $out/bin
+      cp dwarffs $out/bin/
+    '';
 }
