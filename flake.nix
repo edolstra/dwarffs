2f984abe63b6d76bb55c22dc48d61f744ef8ce0b
Eelco Dolstra <edolstra@gmail.com>
2019-07-12 14:22:44 +0200

    Move NixOS module into flake.nix

--- flake.nix
+++ flake.nix
@@ -34,10 +34,29 @@
           '';
       };
 
-    nixosModules.dwarffs = import ./module.nix inputs;
-
     defaultPackage = packages.dwarffs;
 
     checks.build = packages.dwarffs;
+
+    nixosModules.dwarffs =
+      {
+        systemd.packages = [ packages.dwarffs ];
+
+        system.fsPackages = [ packages.dwarffs ];
+
+        systemd.units."run-dwarffs.automount".wantedBy = [ "multi-user.target" ];
+
+        environment.variables.NIX_DEBUG_INFO_DIRS = [ "/run/dwarffs" ];
+
+        systemd.tmpfiles.rules = [ "d /var/cache/dwarffs 0755 dwarffs dwarffs 7d" ];
+
+        users.users.dwarffs =
+          { description = "Debug symbols file system daemon user";
+            group = "dwarffs";
+            isSystemUser = true;
+          };
+
+        users.groups.dwarffs = {};
+      };
   };
 }
