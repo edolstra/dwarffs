50023d28e81470fb135d2605aa573ea20e2717e4
Eelco Dolstra <edolstra@gmail.com>
2019-10-15 18:20:42 +0200

    flake.nix: Support more system types

--- flake.nix
+++ flake.nix
@@ -3,62 +3,72 @@
 
   description = "A filesystem that fetches DWARF debug info from the Internet on demand";
 
-  outputs = { self, nixpkgs }: rec {
+  outputs = { self, nixpkgs }:
 
-    overlay = final: prev: {
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
 
-      dwarffs = with final; stdenv.mkDerivation {
-        name = "dwarffs-0.1.${if self ? lastModified then lib.substring 0 8 self.lastModified else "dirty"}";
+    {
 
-        buildInputs = [ fuse nix nlohmann_json boost ];
+      overlay = final: prev: {
 
-        NIX_CFLAGS_COMPILE = "-I ${nix.dev}/include/nix -include ${nix.dev}/include/nix/config.h -D_FILE_OFFSET_BITS=64";
+        dwarffs = with final; stdenv.mkDerivation {
+          name = "dwarffs-0.1.${if self ? lastModified then lib.substring 0 8 self.lastModified else "dirty"}";
 
-        src = self;
+          buildInputs = [ fuse nix nlohmann_json boost ];
 
-        installPhase =
-          ''
-            mkdir -p $out/bin $out/lib/systemd/system
+          NIX_CFLAGS_COMPILE = "-I ${nix.dev}/include/nix -include ${nix.dev}/include/nix/config.h -D_FILE_OFFSET_BITS=64";
 
-            cp dwarffs $out/bin/
-            ln -s dwarffs $out/bin/mount.fuse.dwarffs
+          src = self;
 
-            cp ${./run-dwarffs.mount} $out/lib/systemd/system/run-dwarffs.mount
-            cp ${./run-dwarffs.automount} $out/lib/systemd/system/run-dwarffs.automount
-          '';
+          installPhase =
+            ''
+              mkdir -p $out/bin $out/lib/systemd/system
+
+              cp dwarffs $out/bin/
+              ln -s dwarffs $out/bin/mount.fuse.dwarffs
+
+              cp ${./run-dwarffs.mount} $out/lib/systemd/system/run-dwarffs.mount
+              cp ${./run-dwarffs.automount} $out/lib/systemd/system/run-dwarffs.automount
+            '';
+        };
+
       };
+
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).dwarffs);
+
+      checks = forAllSystems (system: {
+        build = self.defaultPackage.${system};
+      });
+
+      nixosModules.dwarffs =
+        { pkgs, ... }:
+        {
+          nixpkgs.overlays = [ self.overlay ];
+
+          systemd.packages = [ pkgs.dwarffs ];
+
+          system.fsPackages = [ pkgs.dwarffs ];
+
+          systemd.units."run-dwarffs.automount".wantedBy = [ "multi-user.target" ];
+
+          environment.variables.NIX_DEBUG_INFO_DIRS = [ "/run/dwarffs" ];
+
+          systemd.tmpfiles.rules = [ "d /var/cache/dwarffs 0755 dwarffs dwarffs 7d" ];
+
+          users.users.dwarffs =
+            { description = "Debug symbols file system daemon user";
+              group = "dwarffs";
+              isSystemUser = true;
+            };
+
+          users.groups.dwarffs = {};
+        };
 
     };
-
-    defaultPackage = (import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [ self.overlay ];
-    }).dwarffs;
-
-    checks.build = defaultPackage;
-
-    nixosModules.dwarffs =
-      { pkgs, ... }:
-      {
-        nixpkgs.overlays = [ self.overlay ];
-
-        systemd.packages = [ pkgs.dwarffs ];
-
-        system.fsPackages = [ pkgs.dwarffs ];
-
-        systemd.units."run-dwarffs.automount".wantedBy = [ "multi-user.target" ];
-
-        environment.variables.NIX_DEBUG_INFO_DIRS = [ "/run/dwarffs" ];
-
-        systemd.tmpfiles.rules = [ "d /var/cache/dwarffs 0755 dwarffs dwarffs 7d" ];
-
-        users.users.dwarffs =
-          { description = "Debug symbols file system daemon user";
-            group = "dwarffs";
-            isSystemUser = true;
-          };
-
-        users.groups.dwarffs = {};
-      };
-  };
 }
